c771217c8b631bb89657727506a7a062411fcb1c
jishant07 <jishanta@gmail.com>
2021-04-05 03:33:24 +0530

    Added Firebase read endpoint

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const axios = require('axios')
 
 const firebase = require("firebase");
 require("firebase/firestore");
-
 var admin = require("firebase-admin");
 var serviceAccount = require("./creds.json");
+const { json } = require("body-parser");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -21,18 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
-var date = new Date().toISOString().split("T")[0];
 
 app.get("/", (req, res) => {
-  // db.collection('users').add({
-  //     email:"jishantacharya@gmail.com",
-  //     password:"sjdflkdsjkldsgddsflgkj"
-  // }).then(res =>{
-  //     console.log(res.id)
-  //     db.collection('users').get().then(snapshot =>{
-  //         snapshot.forEach(snap => console.log(snap.data()))
-  //     })
-  // })
   res.send("Hello World");
 });
 
@@ -133,79 +123,11 @@ app.get("/test_endpoint", (req, res) => {
 });
 
 
-// app.get("/schedule_test",(req,res)=>{
-//   var api_key = "&apiKey=187f701335b24b4c8fe92ccb0d639b43";
-//   var country = "?country=in&category=business";
-//   var page_size = "&pageSize=100";
-//   var lang = "&language=en";
-//   var base_url = "https://newsapi.org/v2";
+app.get("/schedule_test",(req,res)=>{
 
-//   var headline_url = base_url+"/top-headlines"+country+page_size+lang+api_key;
-//   var apple = base_url+"/everything?q=apple"+page_size+lang+"&sortBy=publishedAt"+api_key
-//   var tesla = base_url+"/everything?q=tesla"+page_size+lang+"&sortBy=publishedAt"+api_key
-//   var facebook = base_url+"/everything?q=facebook"+page_size+lang+"&sortBy=publishedAt"+api_key
-//   var nvidia = base_url+"/everything?q=nvidia"+page_size+lang+"&sortBy=publishedAt"+api_key
-//   var qualcomm = base_url+"/everything?q=qualcomm"+page_size+lang+"&sortBy=publishedAt"+api_key
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
-//   var req_headlines = axios.get(headline_url)
-//   var req_apple = axios.get(apple)
-//   var req_tesla = axios.get(tesla)
-//   var req_facebook = axios.get(facebook)
-//   var req_nvidia = axios.get(nvidia)
-//   var req_qualcomm = axios.get(qualcomm)
-
-//   axios.all([req_headlines,req_apple,req_tesla,req_facebook,req_nvidia,req_qualcomm])
-//   .then(axios.spread((...result) => {
-//     const headlines_data = result[0].data;
-//     const apple_data = result[1].data;
-//     const tesla_data = result[2].data;
-//     const facebook_data = result[3].data;
-//     const nvidia_data = result[4].data;
-//     const qualcomm_data = result[5].data;
-
-//     var add_headline = new Promise((resolve,reject) => {
-//       db.collection('news')
-//       .doc("headlines_"+date)
-//       .set(headlines_data)
-//       .then(() => resolve("Added Headline"))
-//       .catch(err => reject(err))
-//     })
-//     var add_apple = new Promise((resolve,reject)=>{
-//       db.collection('news')
-//       .doc("apple_"+date)
-//       .set(apple_data)
-//       .then(() => resolve("Added Apple")).catch(err => reject(err))
-//     })
-
-//     var add_tesla = new Promise((resolve,reject)=>{
-//       db.collection("news").doc("tesla_"+date).set(tesla_data)
-//       .then(res => resolve("Added Tesla"))
-//       .catch(err => reject(err))
-//     })
-
-//     var add_facebook = new Promise((resolve,reject) => {
-//       db.collection('news').doc("facebook_"+date).set(facebook_data)
-//       .then(() => resolve("Added Facebook")).catch(err => reject(err))
-//     })
-
-//     var add_nvidia = new Promise((resolve,reject)=>{
-//       db.collection('news').doc("nvidia_"+date).set(nvidia_data)
-//       .then(() => resolve("Added Nvidia")).catch(err => reject(err))
-//     })
-
-//     var add_qualcomm = new Promise((resolve,reject) =>{
-//       db.collection('news').doc("qualcomm_"+date).set(qualcomm_data)
-//       .then(() => resolve("Added Qualcomm")).catch(err => reject(err))
-//     })
-
-//     Promise.all([add_headline,add_apple,add_facebook,add_tesla,add_qualcomm,add_nvidia])
-//     .then(data => res.json({"result_list":data}))
-//     .catch(err => res.json({err}))
-//   }))
-// })
-
-
-cron.schedule("0 9 * * *",()=>{
   var api_key = "&apiKey=187f701335b24b4c8fe92ccb0d639b43";
   var country = "?country=in&category=business";
   var page_size = "&pageSize=100";
@@ -237,42 +159,148 @@ cron.schedule("0 9 * * *",()=>{
 
     var add_headline = new Promise((resolve,reject) => {
       db.collection('news')
-      .doc("headlines_"+date)
-      .set(headlines_data)
+      .add({"index":"headlines_"+date,headlines_data})
       .then(() => resolve("Added Headline"))
       .catch(err => reject(err))
     })
     var add_apple = new Promise((resolve,reject)=>{
       db.collection('news')
-      .doc("apple_"+date)
-      .set(apple_data)
+      .add({"index":"apple_"+date,apple_data})
       .then(() => resolve("Added Apple")).catch(err => reject(err))
     })
 
     var add_tesla = new Promise((resolve,reject)=>{
-      db.collection("news").doc("tesla_"+date).set(tesla_data)
+      db.collection("news")
+      .add({"index":"tesla_"+date,tesla_data})
       .then(res => resolve("Added Tesla"))
       .catch(err => reject(err))
     })
 
     var add_facebook = new Promise((resolve,reject) => {
-      db.collection('news').doc("facebook_"+date).set(facebook_data)
+      db.collection('news').add({"index":"facebook_"+date,facebook_data})
       .then(() => resolve("Added Facebook")).catch(err => reject(err))
     })
 
     var add_nvidia = new Promise((resolve,reject)=>{
-      db.collection('news').doc("nvidia_"+date).set(nvidia_data)
+      db.collection('news').add({"index":"nvidia_"+date,nvidia_data})
       .then(() => resolve("Added Nvidia")).catch(err => reject(err))
     })
 
     var add_qualcomm = new Promise((resolve,reject) =>{
-      db.collection('news').doc("qualcomm_"+date).set(qualcomm_data)
+      db.collection('news').add({"index":"qualcomm_"+date,qualcomm_data})
       .then(() => resolve("Added Qualcomm")).catch(err => reject(err))
     })
 
     Promise.all([add_headline,add_apple,add_facebook,add_tesla,add_qualcomm,add_nvidia])
     .then(data => res.json({"result_list":data}))
     .catch(err => res.json({err}))
+  }))
+})
+
+
+app.post("/read_firebase",async (req,res)=>{
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+  if(req.body.company !== "")
+  {
+    db.collection('news')
+    .where("index","==",req.body.company+"_"+date)
+    .get()
+    .then((snapshot) => {
+      var data = [];
+      if(snapshot.empty){
+        res.json({"msg":"Couldn't find the index"})
+      }
+      else{
+        snapshot.forEach(snap =>{
+          data.push(snap.data())
+        })
+        res.json({
+          data
+        })
+      }
+    })
+  }
+  else{
+    res.json({
+      "msg":"Incomplete Request"
+    })
+  }
+})
+
+cron.schedule("0 9 * * *",()=>{
+
+  console.log("This took place after a minute")
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+  var api_key = "&apiKey=187f701335b24b4c8fe92ccb0d639b43";
+  var country = "?country=in&category=business";
+  var page_size = "&pageSize=100";
+  var lang = "&language=en";
+  var base_url = "https://newsapi.org/v2";
+
+  var headline_url = base_url+"/top-headlines"+country+page_size+lang+api_key;
+  var apple = base_url+"/everything?q=apple"+page_size+lang+"&sortBy=publishedAt"+api_key
+  var tesla = base_url+"/everything?q=tesla"+page_size+lang+"&sortBy=publishedAt"+api_key
+  var facebook = base_url+"/everything?q=facebook"+page_size+lang+"&sortBy=publishedAt"+api_key
+  var nvidia = base_url+"/everything?q=nvidia"+page_size+lang+"&sortBy=publishedAt"+api_key
+  var qualcomm = base_url+"/everything?q=qualcomm"+page_size+lang+"&sortBy=publishedAt"+api_key
+
+  var req_headlines = axios.get(headline_url)
+  var req_apple = axios.get(apple)
+  var req_tesla = axios.get(tesla)
+  var req_facebook = axios.get(facebook)
+  var req_nvidia = axios.get(nvidia)
+  var req_qualcomm = axios.get(qualcomm)
+
+  axios.all([req_headlines,req_apple,req_tesla,req_facebook,req_nvidia,req_qualcomm])
+  .then(axios.spread((...result) => {
+    const headlines_data = result[0].data;
+    const apple_data = result[1].data;
+    const tesla_data = result[2].data;
+    const facebook_data = result[3].data;
+    const nvidia_data = result[4].data;
+    const qualcomm_data = result[5].data;
+
+    var add_headline = new Promise((resolve,reject) => {
+      db.collection('news')
+      .add({"index":"headlines_"+date,headlines_data})
+      .then(() => resolve("Added Headline"))
+      .catch(err => reject(err))
+    })
+    var add_apple = new Promise((resolve,reject)=>{
+      db.collection('news')
+      .add({"index":"apple_"+date,apple_data})
+      .then(() => resolve("Added Apple")).catch(err => reject(err))
+    })
+
+    var add_tesla = new Promise((resolve,reject)=>{
+      db.collection("news")
+      .add({"index":"tesla_"+date,tesla_data})
+      .then(res => resolve("Added Tesla"))
+      .catch(err => reject(err))
+    })
+
+    var add_facebook = new Promise((resolve,reject) => {
+      db.collection('news').add({"index":"facebook_"+date,facebook_data})
+      .then(() => resolve("Added Facebook")).catch(err => reject(err))
+    })
+
+    var add_nvidia = new Promise((resolve,reject)=>{
+      db.collection('news').add({"index":"nvidia_"+date,nvidia_data})
+      .then(() => resolve("Added Nvidia")).catch(err => reject(err))
+    })
+
+    var add_qualcomm = new Promise((resolve,reject) =>{
+      db.collection('news').add({"index":"qualcomm_"+date,qualcomm_data})
+      .then(() => resolve("Added Qualcomm")).catch(err => reject(err))
+    })
+
+    Promise.all([add_headline,add_apple,add_facebook,add_tesla,add_qualcomm,add_nvidia])
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
   }))
 },{scheduled:true,timezone:"Asia/Kolkata"})
 
